eba49b7634a0df1047b5c2154d06db65f30b4c26
tycrek <josh.moore@jmoore.dev>
2021-07-06 14:27:29 -0600

    expose StorageEngine name & version (from package.json)

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const { name: EngineName, version: EngineVersion } = require('./package.json');
 const { StorageEngine, StorageType } = require('./StorageEngine');
 const { StorageFunctionType, StorageFunction, StorageFunctionGroup } = require('./StorageFunction');
 const { KeyFoundError, KeyNotFoundError } = require('./Errors');
 const JsonStorageEngine = require('./JsonStorageEngine');
 
 module.exports = {
+	EngineName,
+	EngineVersion,
 	StorageEngine,
 	StorageType,
 	StorageFunction,
